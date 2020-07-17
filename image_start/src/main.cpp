e7947a27d32fc93022d7efd1a1e1123b9387dd47
Michael LeCompte <michaelslec98@gmail.com>
2020-07-17 15:06:45 -0700

    Refactored code to be perfect in binary

--- image_start/src/main.cpp
+++ image_start/src/main.cpp
@@ -22,70 +22,41 @@ using namespace std;
 
 struct Pixel
 {
-  int red;
-  int green;
-  int blue;
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
 };
 
-int main(int argc, char* argv[]) {
-  
-  //if there are more than 3 command line arguments
-  if (argc > 3 || argc < 3)
-    cerr << "Usage: program file_1 file_2" << endl;
-  
-  //get the name of the file we want to copy data from
-  string file1 = argv[1];
-  //get the name of the file we want to copy data to
-  string file2 = argv[2];
-  
-  // ios::binary - open in binary mode
-  // ios::in - open for input operations
-  ifstream inFile(file1.c_str(), ios::binary | ios::in);
-  
-  if (!inFile)
-    cerr << "Could not open file " << "\"" << file1 << "\"" << endl;
-  
+int main() {
+  ifstream layer_file("input/layer1.tga", ios::binary | ios::in);
+  ifstream pattern_file("input/pattern1.tga", ios::binary | ios::in);
+  ofstream outFile("output/test.tga", ios::binary | ios::out);
+
+  if (!layer_file)
+    cerr << "Could not open file layer1.tga" << endl;
+
+  if (!pattern_file)
+    cerr << "Could not open file layer1.tga" << endl;
+
+  // if the file could not be opened
+  if(!outFile)
+    cerr << "Could not open file test.tga" <<  endl;
   // for storing the read-in information
   Header tgaHeader;
   
-  inFile.read(&tgaHeader.idLength, sizeof(tgaHeader.idLength));
-  inFile.read(&tgaHeader.colorMapType, sizeof(tgaHeader.colorMapType));
-  inFile.read(&tgaHeader.dataTypeCode, sizeof(tgaHeader.dataTypeCode));
-  inFile.read((char*)&tgaHeader.colorMapOrigin, sizeof(tgaHeader.colorMapOrigin));
-  inFile.read((char*)&tgaHeader.colorMapLength, sizeof(tgaHeader.colorMapLength));
-  inFile.read(&tgaHeader.colorMapDepth, sizeof(tgaHeader.colorMapDepth));
-  inFile.read((char*)&tgaHeader.xOrigin, sizeof(tgaHeader.xOrigin));
-  inFile.read((char*)&tgaHeader.yOrigin, sizeof(tgaHeader.yOrigin));
-  inFile.read((char*)&tgaHeader.width, sizeof(tgaHeader.width));
-  inFile.read((char*)&tgaHeader.height, sizeof(tgaHeader.height));
-  inFile.read(&tgaHeader.bitsPerPixel, sizeof(tgaHeader.bitsPerPixel));
-  inFile.read(&tgaHeader.imageDescriptor, sizeof(tgaHeader.imageDescriptor));
-  
-  // create an array of pixels with the length = number of pixels in image
-  unsigned long size = tgaHeader.width * tgaHeader.height;
-  Pixel* pixelData = new Pixel[size];
-  
-  // read in the data for each pixel in the image
-  
-  for (unsigned long i = 0; i < size; i++)
-  {
-    inFile.read((char*)&pixelData[i].red, sizeof(pixelData[i].red));
-    inFile.read((char*)&pixelData[i].green, sizeof(pixelData[i].green));
-    inFile.read((char*)&pixelData[i].blue, sizeof(pixelData[i].blue));
-  }
-  
-  inFile.close(); // close file1
-  
-  //==========================================================================
-  
-  // ios::binary - open in binary mode
-  // ios::in - open for output operations
-  ofstream outFile(file2.c_str(), ios::binary | ios::out);
-  
-  // if the file could not be opened
-  if(!outFile)
-    cerr << "Could not open file " << "\"" << file2 << "\"" <<  endl;
-  
+  layer_file.read(&tgaHeader.idLength, sizeof(tgaHeader.idLength));
+  layer_file.read(&tgaHeader.colorMapType, sizeof(tgaHeader.colorMapType));
+  layer_file.read(&tgaHeader.dataTypeCode, sizeof(tgaHeader.dataTypeCode));
+  layer_file.read((char*)&tgaHeader.colorMapOrigin, sizeof(tgaHeader.colorMapOrigin));
+  layer_file.read((char*)&tgaHeader.colorMapLength, sizeof(tgaHeader.colorMapLength));
+  layer_file.read(&tgaHeader.colorMapDepth, sizeof(tgaHeader.colorMapDepth));
+  layer_file.read((char*)&tgaHeader.xOrigin, sizeof(tgaHeader.xOrigin));
+  layer_file.read((char*)&tgaHeader.yOrigin, sizeof(tgaHeader.yOrigin));
+  layer_file.read((char*)&tgaHeader.width, sizeof(tgaHeader.width));
+  layer_file.read((char*)&tgaHeader.height, sizeof(tgaHeader.height));
+  layer_file.read(&tgaHeader.bitsPerPixel, sizeof(tgaHeader.bitsPerPixel));
+  layer_file.read(&tgaHeader.imageDescriptor, sizeof(tgaHeader.imageDescriptor));
+
   outFile.write(&tgaHeader.idLength, sizeof(tgaHeader.idLength));
   outFile.write(&tgaHeader.colorMapType, sizeof(tgaHeader.colorMapType));
   outFile.write(&tgaHeader.dataTypeCode, sizeof(tgaHeader.dataTypeCode));
@@ -98,16 +69,22 @@ int main(int argc, char* argv[]) {
   outFile.write((char*)&tgaHeader.height, sizeof(tgaHeader.height));
   outFile.write(&tgaHeader.bitsPerPixel, sizeof(tgaHeader.bitsPerPixel));
   outFile.write(&tgaHeader.imageDescriptor, sizeof(tgaHeader.imageDescriptor));
-  
-  // write the data for each pixel in the image to file2
-  for (unsigned long i = 0; i < size; i++)
+
+
+  for (unsigned long i = 0; i < tgaHeader.width * tgaHeader.height; i++)
   {
-    outFile.write((char*)&pixelData[i].red, sizeof(pixelData[i].red));
-    outFile.write((char*)&pixelData[i].green, sizeof(pixelData[i].green));
-    outFile.write((char*)&pixelData[i].blue, sizeof(pixelData[i].blue));
+    Pixel new_pixel;
+
+    layer_file.read((char*)&new_pixel.red, sizeof(new_pixel.red));
+    layer_file.read((char*)&new_pixel.green, sizeof(new_pixel.green));
+    layer_file.read((char*)&new_pixel.blue, sizeof(new_pixel.blue));
+
+    outFile.write((char*)&new_pixel.red, sizeof(new_pixel.red));
+    outFile.write((char*)&new_pixel.green, sizeof(new_pixel.green));
+    outFile.write((char*)&new_pixel.blue, sizeof(new_pixel.blue));
   }
-  
+
+  layer_file.close(); // close file1
+  pattern_file.close();
   outFile.close(); // close file2
-  
-  delete [] pixelData;
 }
