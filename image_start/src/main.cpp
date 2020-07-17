707c2fc5001a9196b04102868b66f418dfb11519
Michael LeCompte <michaelslec98@gmail.com>
2020-07-17 14:11:22 -0700

    Restructured project for tests

--- image_start/src/main.cpp
+++ image_start/src/main.cpp
@@ -1,24 +1,4 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <stack>
-
 using namespace std;
-
-struct Header {
-    char idLength; // 1 byte
-    char colorMapType;
-    char dataTypeCode;
-    short colorMapOrigin; // 2 bytes
-    short colorMapLength;
-    char colorMapDepth;
-    short xOrigin;
-    short yOrigin;
-    short width;
-    short height;
-    char bitsPerPixel;
-    char imageDescriptor;
-}; // 18 bytes total
 
 int main(int argc, char* argv[]) {
 }
---
+++ image_start/tests/tests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Michael on 7/17/2020.
+//
+#include "../src/main.h"
+
+int main() {
+
+  return 0;
+}
