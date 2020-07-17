866744cbcfa113ba5cd275ea740df59f38d0ac21
Michael LeCompte <michaelslec98@gmail.com>
2020-07-17 14:35:32 -0700

    Fixed project structure

--- image_start/src/main.cpp
+++ image_start/src/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stack>
+
+struct Header {
+  char idLength; // 1 byte
+  char colorMapType;
+  char dataTypeCode;
+  short colorMapOrigin; // 2 bytes
+  short colorMapLength;
+  char colorMapDepth;
+  short xOrigin;
+  short yOrigin;
+  short width;
+  short height;
+  char bitsPerPixel;
+  char imageDescriptor;
+}; // 18 bytes total
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
--- image_start/tests/tests.cpp
+++ image_start/tests/tests.cpp
@@ -1,7 +1,6 @@
 //
 // Created by Michael on 7/17/2020.
 //
-#include "../src/main.h"
 
 int main() {
 
